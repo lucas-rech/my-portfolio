d450a03af53732f6ba258f3c3737e7807abfa5bf
Lucas <lucasrech00@gmail.com>
2024-12-12 01:07:37 -0300

    fix: corrigido tamanho da fonte do menu

--- portfolio-client/src/components/Header/LinkMenu.tsx
+++ portfolio-client/src/components/Header/LinkMenu.tsx
@@ -6,7 +6,7 @@ interface LinkMenuProps {
 
 function LinkMenu({title, href, target}: LinkMenuProps) {
   return (
-    <a className="hover:text-lg hover:font-normal focus:text-cyan active:text-beach-blue font-poppins font-light" href={href} target={target}>
+    <a className="hover:text-base hover:font-normal focus:text-cyan active:text-beach-blue font-poppins font-light text-sm" href={href} target={target}>
         <span className="whitespace-nowrap">{title}</span>
     </a>
   )
