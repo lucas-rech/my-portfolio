98b79b1dc01f270f88b1a460e0897f7d653bb77f
Lucas <lucasrech00@gmail.com>
2024-12-12 01:04:09 -0300

    fix: alinhado logo

--- portfolio-client/src/components/Header/Logo.tsx
+++ portfolio-client/src/components/Header/Logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
   return (
     <div className="flex flex-row items-center justify-center gap-4 h-full w-36">
         <img className="w-11" src={logo} alt="logo" />
-        <span className="text-white font-josefin h-fit select-none">Lucas Rech</span>
+        <span className="mt-1 text-white font-josefin h-fit select-none">Lucas Rech</span>
     </div>
   )
 }
